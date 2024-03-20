200899523949f85501c77ebc75514fc58fbf5738
Gemechis Chala <gladsonchala@gmail.com>
2024-03-21 02:36:03 +0300

    few changes

--- background/background.js
+++ background/background.js
@@ -2,23 +2,24 @@
  * Background script to handle background tasks and data synchronization.
  */
 
-// Event listener to handle changes in storage and synchronize data using Chrome sync
+/**
+ *  Event listener to handle changes in storage and synchronize data using Chrome sync
+ */
 chrome.storage.onChanged.addListener((changes, namespace) => {
     for (let key in changes) {
         let storageChange = changes[key];
         if (namespace === 'local') {
-            // Update data in Chrome sync
             chrome.storage.sync.set({ [key]: storageChange.newValue });
         } else if (namespace === 'sync') {
-            // Update data in local storage
             chrome.storage.local.set({ [key]: storageChange.newValue });
         }
     }
 });
 
-// Event listener to handle extension installation and initial setup
+/**
+ * Event listener to handle extension installation and initial setup
+ */
 chrome.runtime.onInstalled.addListener(() => {
-    // Initialize storage if not already initialized
     chrome.storage.local.get(['notes', 'links'], (result) => {
         if (!result.notes) {
             chrome.storage.local.set({ 'notes': [] });
